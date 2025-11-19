3c436bfa01b6bcaee11121830f731df0ad5b6ae9
24023374_LoiYi <24023374@myrp.edu.sg>
2025-11-19 12:27:55 +0800

    Initial commit Initial commit
    Generated by create-expo-app 3.5.3.

--- Lesson06/L6/Car.js
+++ Lesson06/L6/Car.js
@@ -2,22 +2,23 @@ import React, {useState} from 'react';
 import { View, Text, Image } from "react-native";
 import {Picker} from '@react-native-picker/picker';
 
-    const Options = ({firstOption, secondOption, thirdOption, picture, onSelect}) => {
+    const Option = ({firstOption, secondOption, thirdOption, picture, onSelect}) => {
+
         const [selected, setSelected] = useState(' ');
         return (
-            <View style={{marginTop: 10, marginBottom: 10}}>
+            <View style={styles.questionContainer}>
                 <Text>What is this car?</Text>
                 <Picker selectedValue={selected} onValueChange={(value) => {
                     setSelected(value);
                     onSelect(value); }}>
-                    <Picker.Item label='Select an Option' value='Select an Option' />
-                    <Picker.Item label={firstOption} value={firstOption} />
-                    <Picker.Item label={secondOption} value={secondOption} />
-                    <Picker.Item label={thirdOption} value={thirdOption} />
+                    <Picker.Item label='Select an Option' value='Select an Option' style={styles.options}/>
+                    <Picker.Item label={firstOption} value={firstOption} style={styles.options}/>
+                    <Picker.Item label={secondOption} value={secondOption} style={styles.options}/>
+                    <Picker.Item label={thirdOption} value={thirdOption} style={styles.options}/>
                 </Picker>
                 <Image source={picture} style={{ width: 200, height: 120 }}/>
             </View>
         )
     }
 
-export default Options;
+export default Option;
